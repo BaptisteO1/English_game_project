ddc29e42dfb8ee968c8593dbe0b45fe673566b40
BaptisteO1 <baptiste.mr01@gmail.com>
2023-11-23 12:00:26 +0100

    second commit

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function showCodeInput(cardId, correctCode) {
     // Masquer les cartes et afficher le champ de texte
     document.getElementById('cardsContainer').style.display = 'none';
     document.getElementById('codeInput').style.display = 'block';
-
+   
     // Stocker les informations de la carte sélectionnée dans les attributs data
     document.getElementById('codeInput').setAttribute('data-card-id', cardId);
     document.getElementById('codeInput').setAttribute('data-correct-code', correctCode);
@@ -113,3 +113,63 @@ function getRandomColor() {
     // Générer une couleur aléatoire
     return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
+
+
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+    constructor(container, items, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    updateGallery() {
+        this.carouselArray.forEach(el => {
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+            el.classList.remove('gallery-item-6');
+        });
+
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`gallery-item-${i+1}`)
+        })  
+    }
+
+    setCurrentState(direction) {
+        if (direction.className == 'gallery-controls-previous') {
+            this.carouselArray.unshift(this.carouselArray.pop());
+        } else {
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateGallery();
+    }
+
+    setControls() {
+        this.carouselControls.forEach(control => {
+            galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+            document.querySelector(`.gallery-controls-${control}`).innerText = control;
+        })
+    }
+
+    useControls() {
+        const triggers = [...galleryControlsContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+            });
+        });
+    }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
